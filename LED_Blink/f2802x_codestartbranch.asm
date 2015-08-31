9ae5ffb088e90a0f815af1c0054ca7c502ebe545
justloong <liukian@sina.com>
2015-09-01 00:42:38 +0800

    实现把全部程序从FLASH中载入到RAM中

--- LED_Blink/f2802x_codestartbranch.asm
+++ LED_Blink/f2802x_codestartbranch.asm
@@ -35,7 +35,8 @@
 
 WD_DISABLE    .set    1        ;set to 1 to disable WD, else set to 0
 
-    .ref _c_int00
+    ;.ref _c_int00
+    .ref copy_sections
     .global code_start
 
 ***********************************************************************
@@ -50,7 +51,8 @@ code_start:
     .if WD_DISABLE == 1
         LB wd_disable       ;Branch to watchdog disable code
     .else
-        LB _c_int00         ;Branch to start of boot.asm in RTS library
+        ;LB _c_int00         ;Branch to start of boot.asm in RTS library
+    	LB copy_sections
     .endif
 
 ;end codestart section
@@ -62,15 +64,16 @@ code_start:
 ***********************************************************************
     .if WD_DISABLE == 1
 
-    .text
+    ;.text
+    .sect "wddisable"
 wd_disable:
     SETC OBJMODE        ;Set OBJMODE for 28x object code
     EALLOW              ;Enable EALLOW protected register access
     MOVZ DP, #7029h>>6  ;Set data page for WDCR register
     MOV @7029h, #0068h  ;Set WDDIS bit in WDCR to disable WD
     EDIS                ;Disable EALLOW protected register access
-    LB _c_int00         ;Branch to start of boot.asm in RTS library
-
+    ;LB _c_int00         ;Branch to start of boot.asm in RTS library
+	LB copy_sections
     .endif
 
 ;end wd_disable
